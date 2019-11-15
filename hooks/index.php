c41693f4c0f911d03c5f647dd0c53a4443e205e9
chenjing-ayou <1256769172@qq.com>
2019-11-15 15:42:16 +0800

    git自动部署信息

--- hooks/index.php
+++ hooks/index.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('PRC');
 error_reporting(1);
 // 生产环境web目录
 $web_path = '/usr/share/nginx/html/chenjing';
@@ -11,6 +12,8 @@ $fs = fopen('./auto_hook.log', 'a');
 fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
 
 $json_content = file_get_contents('php://input');
+fwrite($fs, 'Data: '.json_encode($json_content).PHP_EOL);
+
 $data = json_decode($json_content, true);
 
 fwrite($fs, 'Data: '.json_encode($data).PHP_EOL);
