21f4e7b3644f1a84e5344580e1cd0ede96ecf944
chenjing-ayou <1256769172@qq.com>
2019-11-15 16:51:06 +0800

    修改

--- hooks/index.php
+++ hooks/index.php
@@ -3,23 +3,21 @@ error_reporting(1);
 // 生产环境web目录
 $web_path = '/usr/share/nginx/html/chenjing';
 
-//作为接口传输的时候认证的密钥
-$valid_token = '!@#Cj121256';
-
 $client_ip = $_SERVER['REMOTE_ADDR'];
 $fs = fopen('./auto_hook.log', 'a');
 fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
 
+// 头部信息
+$data_headers = getallheaders();
+// Payload的数据信息
 $json_content = file_get_contents('php://input');
-$data = json_decode($json_content, true);
+// secret
+$secret_code = '!@#Cj121256';
 
-fwrite($fs, '[ token='.$data['token'].';'.($data['token'] !== $valid_token).']'.PHP_EOL);
-
-$fs and fclose($fs);
-
-if (empty($data['token']) || $data['token'] !== $valid_token) {
-    exit('aInvalid token request');
+$signature = "sha1=".hash_hmac( 'sha1', $json_content,$secret_code);
+if(strcmp($signature, $data_headers['X-Hub-Signature']) == 0){
+    $cmd = "cd $web_path && git pull";
+    shell_exec($cmd);
 }
 
-$cmd = "cd $web_path && git pull";
-shell_exec($cmd);
+
